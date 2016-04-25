63cb5ba1a29ca75af7ac695cd4b0518fdc294835
Herman Bergwerf <hermanbergwerf@gmail.com>
2016-04-25 14:26:45 +0200

    Add all LoadFileParams parameters

--- lib/src/stage.dart
+++ lib/src/stage.dart
@@ -15,8 +15,9 @@ class Stage {
 @anonymous
 @JS()
 class StageParams {
+  /// Constructor
   external factory StageParams(
-      {bool overwritePreferences,
+      {bool overwritePreferences: true,
       String theme,
       String quality,
       bool imposter,
@@ -32,42 +33,66 @@ class StageParams {
       String ambientColor,
       double lightIntensity,
       double ambientIntensity});
+
+  /// Overwrite localStorage preferences with these preferences.
+  /// True by default (it is false by default in the official NGL release)
   external bool get overwritePreferences;
   external set overwritePreferences(bool v);
+
+  /// NGL theme (either `'dark'` or `'light'`)
   external String get theme;
   external set theme(String v);
+
+  /// Render quality
+  ///
+  /// Controls how many vertices are used to render geometries such as cartoons.
+  /// Note that spheres are ray-cased when `EXT_frag_depth` is availabe.
+  /// Use values from [RenderQuality].
   external String get quality;
   external set quality(String v);
-  external bool get imposter;
-  external set imposter(bool v);
+
+  external bool get impostor;
+  external set impostor(bool v);
+
   external double get rotateSpeed;
   external set rotateSpeed(double v);
+
   external double get zoomSpeed;
   external set zoomSpeed(double v);
+
   external double get panSpeed;
   external set panSpeed(double v);
+
   external int get clipNear;
   external set clipNear(int v);
+
   external int get clipFar;
   external set clipFar(int v);
+
   external int get clipDist;
   external set clipDist(int v);
+
   external int get fogNear;
   external set fogNear(int v);
+
   external int get fogFar;
   external set fogFar(int v);
+
   external String get lightColor;
   external set lightColor(String v);
+
   external String get ambientColor;
   external set ambientColor(String v);
+
   external double get lightIntensity;
   external set lightIntensity(double v);
+
   external double get ambientIntensity;
   external set ambientIntensity(double v);
 }
 
 /// Possible values for StageParams.quality
-class Quality {
+class RenderQuality {
   static const low = 'low';
   static const medium = 'medium';
   static const high = 'high';
@@ -77,5 +102,55 @@ class Quality {
 @anonymous
 @JS()
 class LoadFileParams {
-  external factory LoadFileParams({bool defaultRepresentation});
+  external factory LoadFileParams(
+      {bool defaultRepresentation,
+      String name,
+      String ext,
+      bool compressed,
+      bool firstModelOnly,
+      bool asTrajectory,
+      bool cAlphaOnly,
+      bool reorderAtoms,
+      bool dontAutoBond});
+
+  /// Add a set of default representations after loading, defaults to false.
+  external bool get defaultRepresentation;
+  external set defaultRepresentation(bool v);
+
+  /// Name given to the loaded components. Useful for methods
+  /// [getComponentsByName], [getRepresentationsByName] and [getAnythingByName].
+  external String get name;
+  external set name(String v);
+
+  /// File extension to identify file type. By default determined from path or
+  /// file name but can be set explicitly
+  external String get ext;
+  external set ext(String v);
+
+  /// Flag to identify compression type (if any, set to false for none). By
+  /// default determined from path or file name but can be set explicitly.
+  external bool get compressed;
+  external set compressed(bool v);
+
+  /// Load only the first model of a structure file. Defaults to false.
+  external bool get firstModelOnly;
+  external set firstModelOnly(bool v);
+
+  /// Load the structure file as a trajectory: first model as structure and the
+  /// remaining models as coordinate frames. Defaults to false.
+  external bool get asTrajectory;
+  external set asTrajectory(bool v);
+
+  /// Load only the c-alpha atoms of structure file. Defaults to false.
+  external bool get cAlphaOnly;
+  external set cAlphaOnly(bool v);
+
+  /// Reorder atoms (based on modelindex, chainname, resno) to help correctly
+  /// build the structure-model-chain-residue hierarchy. Defaults to false.
+  external bool get reorderAtoms;
+  external set reorderAtoms(bool v);
+
+  /// Prevent automatic bond determination. Defaults to false.
+  external bool get dontAutoBond;
+  external set dontAutoBond(bool v);
 }
--- test/index.dart
+++ test/index.dart
@@ -5,19 +5,11 @@
 import 'package:ngl_viewer/ngl_viewer.dart' as ngl;
 
 void main() {
-  ngl.mainScriptFilePath = 'ngl.embedded.min.js';
+  ngl.mainScriptFilePath = 'packages/ngl_viewer/ngl.embedded.min.js';
 
-  var stage = new ngl.Stage(
-      'ngl-viewer',
-      new ngl.StageParams(
-          theme: 'light',
-          rotateSpeed: 1.0,
-          fogNear: 1,
-          fogFar: 50,
-          lightColor: '#aa0000',
-          quality: ngl.Quality.high,
-          overwritePreferences: true));
+  var stageSettings = new ngl.StageParams(quality: ngl.RenderQuality.low);
+  var loadFileParams = new ngl.LoadFileParams(defaultRepresentation: true);
 
-  stage.loadFile(
-      'rcsb://1blu', new ngl.LoadFileParams(defaultRepresentation: true));
+  var stage = new ngl.Stage('ngl-viewer', stageSettings);
+  stage.loadFile('rcsb://1blu', loadFileParams);
 }
