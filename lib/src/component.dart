c342c7508c1c503cea8e669bfc743a7f6f6264cf
Herman Bergwerf <hermanbergwerf@gmail.com>
2022-05-25 19:03:24 +0200

    Add autoView.

--- lib/src/component.dart
+++ lib/src/component.dart
@@ -3,7 +3,10 @@ part of ngl_viewer;
 @JS()
 class Component {
   external String get name;
-  external List<RepresentationElement> get reprList;
+
   external void removeAllRepresentations();
   external dynamic addRepresentation(String type, params);
+  external List<RepresentationElement> get reprList;
+
+  external void autoView(num duration);
 }
